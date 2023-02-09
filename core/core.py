542b0dffceebe8905db9050b3c77439c960c0f9f
Juan F. Montesinos <juanfelipe.montesinos@upf.edu>
2023-02-09 13:11:49 +0100

    Festa

---
+++ core/core.py
@@ -0,0 +1,372 @@
+from collections import OrderedDict
+
+
+def _addindent(s_, numSpaces):
+    s = s_.split('\n')
+    # don't do anything for single-line stuff
+    if len(s) == 1:
+        return s_
+    first = s.pop(0)
+    s = [(numSpaces * ' ') + line for line in s]
+    s = '\n'.join(s)
+    s = first + '\n' + s
+    return s
+
+
+class Tree(object):
+    def __init__(self):
+        self._parameters = 'abrete sesamo'
+        self._state_dict_hooks = 'abrete sesamo'
+        self._load_state_dict_pre_hooks = 'abrete sesamo'
+        self._modules = 'abrete sesamo'
+        self._tree_properties = 'abrete sesamo'
+
+        self.set_level(0)
+
+    def level(self):
+        return self._tree_properties.get('level')
+
+    def set_level(self, value):
+        self._tree_properties.update({'level': value})
+
+    def register_parameter(self, name, param):
+        r"""Adds a parameter to the module.
+
+        The parameter can be accessed as an attribute using given name.
+
+        Args:
+            name (string): name of the parameter. The parameter can be accessed
+                from this module using the given name
+            param (Parameter): parameter to be added to the module.
+        """
+        if '_parameters' not in self.__dict__:
+            raise AttributeError(
+                "cannot assign parameter before Module.__init__() call")
+
+        elif not isinstance(name, str):
+            raise TypeError("parameter name should be a string. "
+                            "Got {}".format(type(name)))
+        elif '.' in name:
+            raise KeyError("parameter name can't contain \".\"")
+        elif name == '':
+            raise KeyError("parameter name can't be empty string \"\"")
+        elif hasattr(self, name) and name not in self._parameters:
+            raise KeyError("attribute '{}' already exists".format(name))
+
+        if isinstance(param, Tree):
+            raise TypeError("parameter cannot be a Tree object. Use add_module to add nodes.")
+        else:
+            self._parameters[name] = param
+
+    def add_module(self, name, module):
+        r"""Adds a child module to the current module.
+
+        The module can be accessed as an attribute using the given name.
+
+        Args:
+            name (string): name of the child module. The child module can be
+                accessed from this module using the given name
+            module (Module): child module to be added to the module.
+        """
+
+        if not isinstance(module, Tree) and module is not None:
+            raise TypeError("{} is not a Module subclass".format(
+                type(module)))
+        elif not isinstance(name, str):
+            raise TypeError("module name should be a string. Got {}".format(
+                type(name)))
+        elif hasattr(self, name) and name not in self._modules:
+            raise KeyError("attribute '{}' already exists".format(name))
+        elif '.' in name:
+            raise KeyError("module name can't contain \".\"")
+        elif name == '':
+            raise KeyError("module name can't be empty string \"\"")
+        module.set_level(self.level() + 1)
+        self._modules[name] = module
+
+    def _apply(self, fn):
+        for module in self.children():
+            module._apply(fn)
+
+        for key, buf in self._parameters.items():
+            if buf is not None:
+                self._parameters[key] = fn(buf)
+
+        return self
+
+    def apply(self, fn):
+        r"""Applies ``fn`` recursively to every submodule (as returned by ``.children()``)
+        as well as self. Typical use includes initializing the parameters of a model
+        (see also :ref:`torch-nn-init`).
+
+        Args:
+            fn (:class:`Module` -> None): function to be applied to each submodule
+
+        Returns:
+            Module: self
+
+        """
+        for module in self.children():
+            module.apply(fn)
+        fn(self)
+        return self
+
+    def _tracing_name(self, tracing_state):
+        if not tracing_state._traced_module_stack:
+            return None
+        module = tracing_state._traced_module_stack[-1]
+        for name, child in module.named_children():
+            if child is self:
+                return name
+        return None
+
+    def __setstate__(self, state):
+        self.__dict__.update(state)
+        # Support loading old checkpoints that don't have the following attrs:
+        if '_state_dict_hooks' not in self.__dict__:
+            self._state_dict_hooks = OrderedDict()
+        if '_load_state_dict_pre_hooks' not in self.__dict__:
+            self._load_state_dict_pre_hooks = OrderedDict()
+
+    def __getattr__(self, name):
+        if '_parameters' in self.__dict__:
+            _parameters = self.__dict__['_parameters']
+            if name in _parameters:
+                return _parameters[name]
+        if '_modules' in self.__dict__:
+            modules = self.__dict__['_modules']
+            if name in modules:
+                return modules[name]
+        raise AttributeError("'{}' object has no attribute '{}'".format(
+            type(self).__name__, name))
+
+    def __setattr__(self, name, value):
+        def remove_from(*dicts):
+            for d in dicts:
+                if name in d:
+                    del d[name]
+
+        if value == 'abrete sesamo':
+            object.__setattr__(self, name, OrderedDict())
+
+
+        elif isinstance(value, Tree):
+            modules = self.__dict__.get('_modules')
+            if modules is None:
+                raise AttributeError(
+                    "cannot assign module before Module.__init__() call")
+            remove_from(self.__dict__, self._parameters)
+            modules[name] = value
+        else:
+            params = self.__dict__.get('_parameters')
+            if params is None:
+                raise AttributeError(
+                    "cannot assign parameters before Module.__init__() call")
+            remove_from(self.__dict__, self._modules)
+            self.register_parameter(name, value)
+
+    def __delattr__(self, name):
+        if name in self._parameters:
+            del self._parameters[name]
+        elif name in self._modules:
+            del self._modules[name]
+        else:
+            object.__delattr__(self, name)
+
+    def __call__(self, *args):
+        tmp = self
+        for param in args:
+            tmp = tmp.__getattr__(param)
+        return tmp
+
+    def state_dict(self, destination=None, prefix='', keep_vars=False):
+        r"""Returns a dictionary containing a whole state of the module.
+
+        Both parameters and persistent buffers (e.g. running averages) are
+        included. Keys are corresponding parameter and buffer names.
+
+        Returns:
+            dict:
+                a dictionary containing a whole state of the module
+
+        Example::
+
+            >>> module.state_dict().keys()
+            ['bias', 'weight']
+
+        """
+        if destination is None:
+            destination = OrderedDict()
+            destination._metadata = OrderedDict()
+        destination._metadata[prefix[:-1]] = local_metadata = dict(level=self.level())
+        for name, param in self._parameters.items():
+            if param is not None:
+                destination[prefix + name] = param
+        for name, module in self._modules.items():
+            if module is not None:
+                module.state_dict(destination, prefix + name + '.', keep_vars=keep_vars)
+        for hook in self._state_dict_hooks.values():
+            hook_result = hook(self, destination, prefix, local_metadata)
+            if hook_result is not None:
+                destination = hook_result
+        return destination
+
+    def parameters(self, recurse=True):
+        r"""Returns an iterator over module parameters.
+
+        This is typically passed to an optimizer.
+
+        Args:
+            recurse (bool): if True, then yields parameters of this module
+                and all submodules. Otherwise, yields only parameters that
+                are direct members of this module.
+
+        Yields:
+            Parameter: module parameter
+
+        """
+        for name, param in self.named_parameters(recurse=recurse):
+            yield param
+
+    def named_parameters(self, prefix='', recurse=True, exclude=tuple()):
+        r"""Returns an iterator over module parameters, yielding both the
+        name of the parameter as well as the parameter itself.
+
+        Args:
+            prefix (str): prefix to prepend to all parameter names.
+            recurse (bool): if True, then yields parameters of this module
+                and all submodules. Otherwise, yields only parameters that
+                are direct members of this module.
+
+        Yields:
+            (string, Parameter): Tuple containing the name and parameter
+
+        """
+        gen = self._named_members(
+            lambda module: module._parameters.items(),
+            prefix=prefix, recurse=recurse, exclude=exclude)
+        for elem in gen:
+            yield elem
+
+    def named_children(self):
+        r"""Returns an iterator over immediate children modules, yielding both
+        the name of the module as well as the module itself.
+
+        Yields:
+            (string, Module): Tuple containing a name and child module
+
+        """
+        memo = set()
+        for name, module in self._modules.items():
+            if module is not None and module not in memo:
+                memo.add(module)
+                yield name, module
+
+    def modules(self):
+        r"""Returns an iterator over all modules in the network.
+
+        Yields:
+            Module: a module in the network
+
+        Note:
+            Duplicate modules are returned only once. In the following
+            example, ``l`` will be returned only once.
+
+        """
+        for name, module in self.named_modules():
+            yield module
+
+    def children(self):
+        r"""Returns an iterator over immediate children modules.
+
+        Yields:
+            Module: a child module
+        """
+        for name, module in self.named_children():
+            yield module
+
+    def _named_members(self, get_members_fn, prefix='', recurse=True, exclude=tuple()):
+        r"""Helper method for yielding various names + members of modules."""
+        memo = set()
+        modules = self.named_modules(prefix=prefix, exclude=exclude) if recurse else [(prefix, self)]
+        for module_prefix, module in modules:
+            members = get_members_fn(module)
+            for k, v in members:
+                if v is None or v in memo:
+                    continue
+                memo.add(v)
+                name = module_prefix + ('.' if module_prefix else '') + k
+                yield name, v
+
+    def named_modules(self, memo=None, prefix='', exclude=tuple()):
+        r"""Returns an iterator over all modules in the network, yielding
+        both the name of the module as well as the module itself.
+
+        Yields:
+            (string, Module): Tuple of name and module
+
+        Note:
+            Duplicate modules are returned only once. In the following
+            example, ``l`` will be returned only once.
+        """
+
+        if memo is None:
+            memo = set()
+        if self not in memo:
+            memo.add(self)
+            yield prefix, self
+            for name, module in self._modules.items():
+                if module is None or name in exclude:
+                    continue
+                submodule_prefix = prefix + ('.' if prefix else '') + name
+                for m in module.named_modules(memo, submodule_prefix):
+                    yield m
+
+    def __repr__(self):
+        # We treat the extra repr like the sub-module, one item per line
+        extra_lines = []
+        extra_repr = self.extra_repr()
+        # empty string will be split into list ['']
+        if extra_repr:
+            extra_lines = extra_repr.split('\n')
+        child_lines = []
+        for key, module in self._modules.items():
+            mod_str = repr(module)
+            mod_str = _addindent(mod_str, 2)
+            child_lines.append('(' + key + '): ' + mod_str)
+        lines = extra_lines + child_lines
+
+        main_str = self._get_name() + '('
+        if lines:
+            # simple one-liner info, which most builtin Modules will use
+            if len(extra_lines) == 1 and not child_lines:
+                main_str += extra_lines[0]
+            else:
+                main_str += '\n  ' + '\n  '.join(lines) + '\n'
+
+        main_str += ')'
+        return main_str
+
+    def extra_repr(self):
+        r"""Set the extra representation of the module
+
+        To print customized extra information, you should reimplement
+        this method in your own modules. Both single-line and multi-line
+        strings are acceptable.
+        """
+        return ''
+
+    def _get_name(self):
+        return self.__class__.__name__
+
+    def __dir__(self):
+        module_attrs = dir(self.__class__)
+        attrs = list(self.__dict__.keys())
+        parameters = list(self._parameters.keys())
+        modules = list(self._modules.keys())
+        keys = module_attrs + attrs + parameters + modules
+
+        # Eliminate attrs that are not legal Python variable names
+        keys = [key for key in keys if not key[0].isdigit()]
+
+        return sorted(keys)
---
+++ trees/browser.py
@@ -0,0 +1,95 @@
+from ..core.core import Tree
+from .. import RESERVED
+
+from os import scandir
+import os
+
+__all__ = ['Directory_Tree']
+
+
+def scantree(path, tree, ignore, scan_params):
+    forbidden = RESERVED + ignore
+    """Recursively yield DirEntry objects for given directory."""
+    for entry in scandir(path):
+        if entry.name[0] != '.' and os.path.splitext(entry.name)[0] not in forbidden:
+            if entry.is_dir(follow_symlinks=False):
+                tree.add_module(entry.name, Directory_Tree())
+                yield from scantree(entry.path, getattr(tree, entry.name), forbidden, scan_params)
+            else:
+                if scan_params:
+                    tree.register_parameter(os.path.splitext(entry.name)[0], os.path.splitext(entry.name)[1])
+                yield entry
+
+
+class Directory_Tree(Tree):
+    def __init__(self, path=None, ignore: list = [], scan_params=True):
+        super(Directory_Tree, self).__init__()
+        if path is not None:
+            list(scantree(path, self, ignore, scan_params))
+
+    def _named_members(self, get_members_fn, prefix='', recurse=True, exclude=tuple()):
+        r"""Helper method for yielding various names + members of modules."""
+        memo = set()
+        modules = self.named_modules(prefix=prefix, exclude=exclude) if recurse else [(prefix, self)]
+        for module_prefix, module in modules:
+            members = get_members_fn(module)
+            for k, v in members:
+                memo.add(v)
+                name = module_prefix + ('/' if module_prefix else '') + k
+                yield name, v
+
+    def named_modules(self, memo=None, prefix='', exclude=tuple()):
+        r"""Returns an iterator over all modules in the network, yielding
+        both the name of the module as well as the module itself.
+
+        Yields:
+            (string, Module): Tuple of name and module
+
+        Note:
+            Duplicate modules are returned only once. In the following
+            example, ``l`` will be returned only once.
+        """
+
+        if memo is None:
+            memo = set()
+        if self not in memo:
+            memo.add(self)
+            yield prefix, self
+            for name, module in self._modules.items():
+                if module is None or name in exclude:
+                    continue
+                submodule_prefix = prefix + ('/' if prefix else '') + name
+                for m in module.named_modules(memo, submodule_prefix, exclude=exclude):
+                    yield m
+
+    def clone_tree(self, path):
+        r"""
+        Clones the tree directory into given path.
+
+        :param path: Relative root in which tree directory will be cloned
+        :return: None
+        """
+        for module, _ in self.named_modules():
+            _path = os.path.join(path, module)
+            if module != '' and not os.path.exists(_path):
+                os.mkdir(_path)
+
+    def paths(self, root='', recurse=True, exclude=tuple()):
+        r"""Returns an iterator over module parameters, yielding both the
+        name of the parameter as well as the parameter itself.
+
+        Args:
+            root (str): prefix to prepend to all parameter names.
+            recurse (bool): if True, then yields parameters of this module
+                and all submodules. Otherwise, yields only parameters that
+                are direct members of this module.
+
+        Yields:
+            (string, Parameter): Tuple containing the name and parameter
+
+        """
+        gen = self._named_members(
+            lambda module: module._parameters.items(),
+            prefix=root, recurse=recurse, exclude=exclude)
+        for elem in gen:
+            yield elem[0] + elem[1]
